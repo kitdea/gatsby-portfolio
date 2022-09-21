53872f03489b4b0b68811a1da805952a31e79f32
kitdea <francismarrosales@gmail.com>
2022-09-21 21:06:42 +0800

    add gatsby-contentful repo

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,7 @@ module.exports = {
     resolve: 'gatsby-source-contentful',
     options: {
       "accessToken": "wdrVuIOyx97C8mh9cAE0VEKU9BfQCC7oCcRYeo1ZZAA",
-      "spaceId": ""
+      "spaceId": "1daipl7z93ig"
     }
   }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-sass", {
     resolve: 'gatsby-source-filesystem',
